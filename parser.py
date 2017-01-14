ea44955f999c3800ce5ffba5c6499c5d22f86ff7
aashrita <aashrita4@gmail.com>
2017-01-13 16:04:24 -0800

    added BTagging code

--- parser.py
+++ parser.py
@@ -14,9 +14,11 @@ import rootpy.plotting.root2matplotlib as rplt
 
 from parser_constants import * # get constants used in this file
 
+
+
 '''
 
-fill_tree(nentries, tree, leaf, numJets, minJet, maxJet)
+fill_JetPT_tree(nentries, tree, leaf, numJets, minJet, maxJet)
 
 Fills gives tree over nentries given. Iterates through leaf values to extract
 numJets, minJet, and maxJet values to fill. Normalizes the numJets, minJet, 
@@ -24,7 +26,7 @@ and maxJet histograms.
 
 '''
 
-def fill_tree(nentries, tree, leaf, numJets, minJet, maxJet):
+def fill_JetPT_tree(nentries, tree, leaf, numJets, minJet, maxJet):
     
     # to loop over all entries in the tree:
     for e in range(nentries):
@@ -75,16 +77,19 @@ def fill_tree(nentries, tree, leaf, numJets, minJet, maxJet):
     maxJet.Scale(1/norm)
     minJet.Scale(1/norm)
 
+
+
+
 '''
 
-simple_delphes_parser(f_tt, f_qcd, f_wjet)
+extract_JetPT(f_tt, f_qcd, f_wjet)
 
 This is the general function dealing with Jet.PT data. Get trees from files and 
 create histograms. Fill necessary trees, and create plots.
 
 '''
    
-def simple_delphes_parser(f_tt, f_qcd, f_wjet):
+def extract_JetPT(f_tt, f_qcd, f_wjet):
     
     # get trees from files
     t_tt = f_tt.Get("Delphes")
@@ -124,9 +129,9 @@ def simple_delphes_parser(f_tt, f_qcd, f_wjet):
 
    
     # FILLING THE TREE
-    fill_tree(tt_n_entries, t_tt, leaf_tt, numJets_tt, min_jetpt_per_event_tt, max_jetpt_per_event_tt)
-    fill_tree(qcd_n_entries, t_qcd, leaf_qcd, numJets_qcd, min_jetpt_per_event_qcd, max_jetpt_per_event_qcd)
-    fill_tree(wjet_n_entries, t_wjet, leaf_wjet, numJets_wjet, min_jetpt_per_event_wjet, max_jetpt_per_event_wjet)
+    fill_JetPT_tree(tt_n_entries, t_tt, leaf_tt, numJets_tt, min_jetpt_per_event_tt, max_jetpt_per_event_tt)
+    fill_JetPT_tree(qcd_n_entries, t_qcd, leaf_qcd, numJets_qcd, min_jetpt_per_event_qcd, max_jetpt_per_event_qcd)
+    fill_JetPT_tree(wjet_n_entries, t_wjet, leaf_wjet, numJets_wjet, min_jetpt_per_event_wjet, max_jetpt_per_event_wjet)
     
     #set line colors
     numJets_tt.SetLineColor('blue')
@@ -216,6 +221,185 @@ def simple_delphes_parser(f_tt, f_qcd, f_wjet):
     #make the plots wait on screen
     wait(True)
     
+
+
+'''
+
+fill_JetBTag_tree(nentries, tree, leaf, numJets, minJet, maxJet)
+
+Fills gives tree over nentries given. Iterates through leaf values to extract
+numJets, minJet, and maxJet values to fill. Normalizes the numJets, minJet, 
+and maxJet histograms.
+
+'''
+
+def fill_JetBTag_tree(nentries, tree, leaf, numLoose, numMedium, numTight):
+    
+    # to loop over all entries in the tree:
+    for e in range(nentries):
+        entry = tree.GetEntry(e)
+        
+        # counter for number of values per entry > JETPT_THRESHOLD
+        cntr_tight = 0
+        cntr_medium = 0
+        cntr_loose = 0
+        
+       
+        #print "MIN: %f MAX: %f" % (minjetpt, maxjetpt)
+        
+        #iterate through each value in leaf of jet pts
+        for i in range(leaf.GetLen()):
+            curr_val = int (leaf.GetValue(i)) # get the value
+            
+            if (curr_val & (1 << BIT_TIGHT)):
+                cntr_tight += 1
+            elif (curr_val & (1 << BIT_MEDIUM)):
+                cntr_medium += 1
+            elif (curr_val & (1 << BIT_LOOSE)):
+                cntr_loose += 1
+        
+       
+       #if (minjetpt < 10.0):       
+            #print "MIN: %f MAX: %f" % (minjetpt, maxjetpt)
+        
+        # get number of jets with jetpt > JETPT_THRESHOLD        
+        numTight.Fill(cntr_tight) 
+        numMedium.Fill(cntr_medium)
+        numLoose.Fill(cntr_loose) 
+        
+    # get float version of num entries to normalize below; subtract 1 to get 
+    # actual integral value of hist
+    norm = float(nentries - 1)
+    
+    # normalize
+    numTight.Scale(1/norm)
+    numMedium.Scale(1/norm)
+    numLoose.Scale(1/norm)
+
+
+
+
+'''
+
+extract_JetBTag(f_tt, f_qcd, f_wjet)
+
+This is the general function dealing with Jet.PT data. Get trees from files and 
+create histograms. Fill necessary trees, and create plots.
+
+'''
+   
+def extract_JetBTag(f_tt, f_qcd, f_wjet):
+
+    # get trees from files
+    t_tt = f_tt.Get("Delphes")
+    t_qcd = f_qcd.Get("Delphes")
+    t_wjet = f_wjet.Get("Delphes")
+    
+    # get number of entries
+    tt_n_entries = t_tt.GetEntries()
+    qcd_n_entries = t_qcd.GetEntries()
+    wjet_n_entries = t_wjet.GetEntries()
+    
+    # define leaves
+    var_tt = "Jet.PT"
+    var_qcd = "Jet.PT"
+    var_wjet = "Jet.PT"
+    
+    leaf_tt = t_tt.GetLeaf(var_tt)
+    leaf_qcd = t_qcd.GetLeaf(var_qcd)
+    leaf_wjet = t_wjet.GetLeaf(var_wjet)
+   
+    
+    # create the histograms
+    loose_tt = Hist(NBINS,NLO,NHI, title = 'loose_tt', legendstyle = 'L')
+    medium_tt = Hist(NBINS,NLO,NHI, title = 'medium_tt', legendstyle = 'L')
+    tight_tt = Hist(NBINS,NLO,NHI, title = 'tight_tt', legendstyle = 'L')
+    
+    loose_qcd = Hist(NBINS,NLO,NHI, title = 'loose_qcd', legendstyle = 'L')
+    medium_qcd = Hist(NBINS,NLO,NHI, title = 'medium_qcd', legendstyle = 'L')
+    tight_qcd = Hist(NBINS,NLO,NHI, title = 'tight_qcd', legendstyle = 'L')
+    
+    loose_wjet = Hist(NBINS,NLO,NHI, title = 'loose_wjet', legendstyle = 'L')
+    medium_wjet = Hist(NBINS,NLO,NHI, title = 'medium_wjet', legendstyle = 'L')
+    tight_wjet = Hist(NBINS,NLO,NHI, title = 'tight_wjet', legendstyle = 'L')
+
+   
+    # FILLING THE TREE
+    fill_JetBTag_tree(tt_n_entries, t_tt, leaf_tt, loose_tt, medium_tt, tight_tt)
+    fill_JetBTag_tree(qcd_n_entries, t_qcd, leaf_qcd, loose_qcd, medium_qcd, tight_qcd)
+    fill_JetBTag_tree(wjet_n_entries, t_wjet, leaf_wjet, loose_wjet, medium_wjet, tight_wjet)
+    
+    #set line colors
+    loose_tt.SetLineColor('blue')
+    medium_tt.SetLineColor('green')
+    tight_tt.SetLineColor('red')
+    
+    loose_qcd.SetLineColor('blue')
+    medium_qcd.SetLineColor('green')
+    tight_qcd.SetLineColor('red')
+    
+    loose_wjet.SetLineColor('blue')
+    medium_wjet.SetLineColor('green')
+    tight_wjet.SetLineColor('red')
+       
+    
+    #begin drawing stuff
+    c1 = Canvas()
+    loose_tt.SetStats(0)
+    loose_tt.Draw('HIST')
+    medium_tt.Draw('HIST SAME')
+    tight_tt.Draw('HIST SAME')
+    
+    
+    #make legend
+    l1 = Legend([loose_tt, medium_tt, tight_tt], textfont = 42, textsize = .03)
+    l1.Draw()
+    
+    #save as pdf
+    c1.SaveAs("btag_tt.pdf");
+    
+    
+    
+    c2 = Canvas()
+    loose_qcd.SetStats(0)
+    loose_qcd.Draw('HIST')
+    medium_qcd.Draw('HIST SAME')
+    tight_qcd.Draw('HIST SAME')
+    
+    
+    #make legend
+    l2 = Legend([loose_qcd, medium_qcd, tight_qcd], textfont = 42, textsize = .03)
+    l2.Draw()
+    
+    #save as pdf
+    c2.SaveAs("btag_qcd.pdf");
+    
+    
+    
+    
+    c3 = Canvas()
+    loose_wjet.SetStats(0)
+    loose_wjet.Draw('HIST')
+    medium_wjet.Draw('HIST SAME')
+    tight_wjet.Draw('HIST SAME')
+    
+    
+    #make legend
+    l3 = Legend([loose_wjet, medium_wjet, tight_wjet], textfont = 42, textsize = .03)
+    l3.Draw()
+    
+    #save as pdf
+    c3.SaveAs("btag_wjet.pdf");
+    
+
+
+
+
+
+
+
+
+
     
    
 def main():
@@ -226,7 +410,8 @@ def main():
     f_wjet = rt.TFile("../../ttbar_data/wjets_lepFilter_13TeV_3.root")
     
     # run functions you want here
-    simple_delphes_parser(f_tt, f_qcd, f_wjet)
+    #extract_JetPT(f_tt, f_qcd, f_wjet)
+    extract_JetBTag(f_tt, f_qcd, f_wjet)
                  
 
 if __name__ == "__main__":
